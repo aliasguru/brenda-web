691b7c9c64c7cfb792ba051800d591fca3fc16df
aliasguru <message@aliasguru.eu>
2018-09-01 12:19:25 +0200

    fix p3 detection, raine core limit
    want all p3 gpu, but only the 32 core and up cpu nodes

--- instance_list/aws_instance_parser.py
+++ instance_list/aws_instance_parser.py
@@ -1,12 +1,12 @@
-#    Download https://pricing.us-east-1.amazonaws.com/offers/v1.0/aws/AmazonEC2/current/index.json to local directory as index.json
+#    	Download https://pricing.us-east-1.amazonaws.com/offers/v1.0/aws/AmazonEC2/current/index.json to local directory as index.json
 #
-#    Install pandas
+#    	Install pandas
 
 import json
 import pandas as pd
 
 __output_file__ = '../app/instances.json'
-__min_number_of_cores__ = 16
+__min_number_of_cores__ = 32
 
 f = open("index.json", "r")
 
@@ -17,32 +17,33 @@ data = []
 
 for ld in lumpy_data:
 	try:
-	    i = {k: v for k, v in ld["attributes"].items()}
-	    i["sku"] = ld["sku"]
-	    i["productFamily"] = ld["productFamily"]
-	    data.append(i)
+		i = {k: v for k, v in ld["attributes"].items()}
+		i["sku"] = ld["sku"]
+		i["productFamily"] = ld["productFamily"]
+		data.append(i)
 	except:
 		pass
 
 df = pd.DataFrame(data)
 
-#    [nan, u'AWS GovCloud (US)', u'Asia Pacific (Mumbai)', u'Asia Pacific (Seoul)', u'Asia Pacific (Singapore)',
-#    u'Asia Pacific (Sydney)', u'Asia Pacific (Tokyo)', u'Canada (Central)', u'EU (Frankfurt)', u'EU (Ireland)',
-#    u'EU (London)', u'South America (Sao Paulo)', u'US East (N. Virginia)', u'US East (Ohio)',
-#    u'US West (N. California)', u'US West (Oregon)']
+#    	[nan, u'AWS GovCloud (US)', u'Asia Pacific (Mumbai)', u'Asia Pacific (Seoul)', u'Asia Pacific (Singapore)',
+#    	u'Asia Pacific (Sydney)', u'Asia Pacific (Tokyo)', u'Canada (Central)', u'EU (Frankfurt)', u'EU (Ireland)',
+#    	u'EU (London)', u'South America (Sao Paulo)', u'US East (N. Virginia)', u'US East (Ohio)',
+#    	u'US West (N. California)', u'US West (Oregon)']
 
 df["location"] = df["location"].map({
-    "US East (Ohio)": "us-east-2", "US East (N. Virginia)": "us-east-1", "US West (N. California)": "us-west-1",
-    "US West (Oregon)": "us-west-2", "Asia Pacific (Mumbai)": "ap-south-1", "Asia Pacific (Seoul)": "ap-northeast-2",
-    "Asia Pacific (Singapore)": "ap-southeast-1", "Asia Pacific (Sydney)": "ap-southeast-2",
-    "Asia Pacific (Tokyo)": "ap-northeast-1", "Canada (Central)": "ca-central-1", "EU (Frankfurt)": "eu-central-1",
-    "EU (Ireland)": "eu-west-1", "EU (London)": "eu-west-2", "South America (Sao Paulo)": "sa-east-1"})
+	"US East (Ohio)": "us-east-2", "US East (N. Virginia)": "us-east-1", "US West (N. California)": "us-west-1",
+	"US West (Oregon)": "us-west-2", "Asia Pacific (Mumbai)": "ap-south-1", "Asia Pacific (Seoul)": "ap-northeast-2",
+	"Asia Pacific (Singapore)": "ap-southeast-1", "Asia Pacific (Sydney)": "ap-southeast-2",
+	"Asia Pacific (Tokyo)": "ap-northeast-1", "Canada (Central)": "ca-central-1", "EU (Frankfurt)": "eu-central-1",
+	"EU (Ireland)": "eu-west-1", "EU (London)": "eu-west-2", "South America (Sao Paulo)": "sa-east-1"})
 
 _instanceData = df[(df["operatingSystem"] == "Linux")
 				& (df["location"].notnull())
 				& (df["productFamily"] == "Compute Instance")
-				& (df['vcpu'].astype(float) >= __min_number_of_cores__)][["location", "instanceType"]].\
-			drop_duplicates().to_json(orient = "records")
+				& ((df['vcpu'].astype(float) >= __min_number_of_cores__)
+				| (df['instanceType'].str.contains('p3')))][["location", "instanceType"]].\
+				drop_duplicates().to_json(orient = "records")
 
 try:
 	_t = open(__output_file__, 'w')
